feb9bc52b233a75bbe41767861e0bd77c2f7e080
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-05-08 14:42:07 -0300

    solve issue when checking output from MCMC module before cleaning chain from burned steps

--- src/features/clean_chain.py
+++ src/features/clean_chain.py
@@ -7,6 +7,7 @@ import sys
 from pathlib import Path
 
 import emcee
+import h5py
 import numpy as np
 import poskiorb
 import yaml
@@ -60,26 +61,28 @@ def main(config_file: str = "") -> None:
     # set some constant values
     nburn = config["MCMC"].get("burn")
     filename = config["MCMC"].get("filename")
+    output_filename = config["MCMC"].get("processed_filename")
     priorsD = config["MCMC"].get("priorDistributions")
     # Cygnus X-1 properties
     stellarParameters = config["StellarParameters"]
 
     # load samples & remove burned steps
-    print("loading MCMC chain * burning steps", end="... ")
+    print("loading MCMC chain and burning steps", end="... ")
     reader = emcee.backends.HDFBackend(filename, read_only=True)
     samples = reader.get_chain(flat=True, discard=nburn)
     # to make plots we don't need more than 10 000 samples, choose them randomly
     samples_r = samples[np.random.choice(samples.shape[0], 10000, replace=False), :]
     print("done !")
 
+    print("samples pre-CC shape:", samples_r.shape, type(samples_r))
+
     kwargs = dict()
     kwargs.update(stellarParameters)
     kwargs.update(priorsD)
 
-    samples2 = []
-
     # evaluate kicks model
     print("computing binary stellar parameters after kick", end="... ", flush=True)
+    samples1, samples2 = [], []
     for k in range(len(samples_r)):
         # replace values of theta and phi outside of boundaries:
         # 0 < theta < pi, and 0 < phi < 2 * pi
@@ -106,18 +109,12 @@ def main(config_file: str = "") -> None:
             a=poskiorb.utils.P_to_a(samples_r[k, 0], samples_r[k, 1], samples_r[k, 2]),
             m1=samples_r[k, 1],
             m2=samples_r[k, 2],
-            m1_remnant_mass=21,
+            m1_remnant_mass=kwargs["M_BH"],
             w=samples_r[k, 3],
             theta=samples_r[k, 4],
             phi=samples_r[k, 5],
             ids=np.ones(1),
         )
-
-        print("main:", a_post, p_post, e, cos_i, v_sys)
-
-        # patch for unbounded binaries
-        if len(e) == 0:
-            continue
 
         # compute likelihood
         args = [
@@ -133,14 +130,23 @@ def main(config_file: str = "") -> None:
         if not np.isfinite(ll):
             continue
 
+        samples1.append(
+            [float(samples_r[k,0]), float(poskiorb.utils.P_to_a(samples_r[k, 0], samples_r[k, 1], samples_r[k, 2])),
+             float(samples_r[k,1]), float(samples_r[k,2]), float(samples_r[k,3]), float(samples_r[k,4]), float(samples_r[k,5])]
+        )
         samples2.append(
             [float(p_post), float(e), np.rad2deg(np.arccos(float(cos_i))), float(v_sys), ll]
         )
-
     print("done !")
+
     # use numpy arrays
+    samples_pre = np.asarray(samples1)
     samples_post = np.asarray(samples2)
-    print("samples post-CC shape:", samples_post.shape)
+    print("sample shapes pre, post-CC:", samples_pre.shape, samples_post.shape)
+
+    with h5py.File(output_filename, "w") as f:
+        f.create_dataset("mcmc/pre-cc", data=samples_pre, compression="gzip")
+        f.create_dataset("mcmc/post-cc", data=samples_post, compression="gzip")
 
 
 if __name__ == "__main__":
